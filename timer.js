2344039a864ce8d208e1fd82ba0dc15642338d25
Carlos <soto.carlosf@gmail.com>
2018-10-08 10:22:09 -0500

    actualizacion de condiciones en control

---
+++ timer.js
@@ -0,0 +1,84 @@
+timer_par = {
+    fs: {
+        led: {
+            start_hour: 11,
+            start_minute: 53,
+            finish_hour: 11,
+            finish_minute: 55
+        },
+        vent: {
+            start_hour: 11,
+            start_minute: 54,
+            finish_hour: 11,
+            finish_minute: 56
+        },
+        warm: {
+            start_hour: 11,
+            start_minute: 54,
+            finish_hour: 11,
+            finish_minute: 56
+        }
+    },
+    rs: {
+        led: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 25
+        },
+        vent: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 24
+        },
+        warm: {
+            start_hour: 10,
+            start_minute: 19,
+            finish_hour: 10,
+            finish_minute: 22
+        }
+    },
+    ws: {
+        led: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 25
+        },
+        vent: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 24
+        },
+        warm: {
+            start_hour: 10,
+            start_minute: 19,
+            finish_hour: 10,
+            finish_minute: 22
+        }
+    },
+    bs: {
+        led: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 25
+        },
+        vent: {
+            start_hour: 10,
+            start_minute: 22,
+            finish_hour: 10,
+            finish_minute: 24
+        },
+        warm: {
+            start_hour: 10,
+            start_minute: 19,
+            finish_hour: 10,
+            finish_minute: 22
+        }
+    }
+}
+
+export { timer_par };
